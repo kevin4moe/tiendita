8ac75a09e19bd2d45ef5b75ce7803f6aec4245b3
kevin4moe <kevin4moe@gmail.com>
2023-02-15 20:55:16 -0600

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// Description: This is the entry point for the application
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
