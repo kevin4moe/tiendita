1505527ff55d2ef9d95f09e1e7e67996aeea5bd1
kevin4moe <kevin4moe@gmail.com>
2023-02-21 10:38:12 -0600

    Add Astro frontend

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Description: This is the entry point for the application
 import express from 'express';
+import { handler as ssrHandler } from './web/dist/server/entry.mjs';
 import routerApi from './routes/index.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.static('web/dist/client/'));
+app.use(ssrHandler);
 
 routerApi(app);
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
-import { Router, static as expressStatic } from 'express';
+import { Router } from 'express';
 import usersRouter from './users.router.js';
 import categoriesRouter from './categories.router.js';
 import productsRouter from './products.router.js';
-import homeRouter from './home.router.js';
 
 function routerApi(app) {
   const router = Router();
-  app.use(expressStatic('views'));
-
-  app.use('/', homeRouter);
 
   app.use('/api/v1', router);
   router.use('/users', usersRouter);
