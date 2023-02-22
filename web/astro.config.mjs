b6abbbe2fcccfc30c63437e4bef289c61f5b4df0
kevin4moe <kevin4moe@gmail.com>
2023-02-21 22:27:46 -0600

    config prettier

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    tailwind({
+      config: { path: "./tailwind.config.cjs" },
+    }),
+  ],
   output: "server",
   adapter: node({
     mode: "middleware",
