05e05fd51a7ad72df790146128e2e4b4970a31bb
kevin4moe <kevin4moe@gmail.com>
2023-03-05 12:27:27 -0600

    Settings update

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    vue(),
     tailwind({
       config: { path: "./tailwind.config.cjs" },
     }),
