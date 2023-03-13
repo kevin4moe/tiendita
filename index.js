fbcf3d85fdb98c643866b99011df4409b6cdd4a6
kevin4moe <kevin4moe@gmail.com>
2023-03-13 08:54:32 -0600

    All astro dependency must be inside web folder

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { handler as ssrHandler } from './web/dist/server/entry.mjs';
-// import routerApi from './routes/index.js';
+import routerApi from './routes/index.js';
 
-// import {
-//   logErrors,
-//   errorHandler,
-//   boomErrorHandler,
-// } from './middleware/error.handler.js';
+import {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} from './middleware/error.handler.js';
 
 const app = express();
 const port = 3000;
@@ -18,10 +18,10 @@ app.use(cookieParser('secret'));
 app.use(express.static('./web/dist/client/'));
 app.use(ssrHandler);
 
-// routerApi(app);
+routerApi(app);
 
-// app.use(logErrors);
-// app.use(boomErrorHandler);
-// app.use(errorHandler);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
